34aca8c080860d91ca30930500f83e155549d100
landa03 <60487799+landa03@users.noreply.github.com>
2020-09-30 15:59:32 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Quik_sort
+{
+    class Program
+    {
+        //cuantos numeros hay
+        public static decimal numMax;
+        //numero seleccionado
+        public static int numSelec;
+        //numero de enmedio
+        public static decimal numMid;
+
+        static void Ges()
+        {
+            numMid = Math.Round(numMax/2);
+            System.Console.WriteLine(numMid);
+            QuikSort();
+        }
+        static void QuikSort()
+        {   
+            if (numMid > numSelec)
+            {
+                numMax = numMid;
+                Ges();
+            }
+            if (numMid < numSelec)
+            {
+                numMid =(numMid+(numMid/2));
+                System.Console.WriteLine(numMid);
+                QuikSort();
+            }
+            else
+            {
+                System.Console.WriteLine($"Es este? {numMid}");
+            }
+        }
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("cuantos numeros hay?");
+            numMax = Convert.ToDecimal(Console.ReadLine());
+            System.Console.WriteLine("cual numero buscas?");
+            numSelec = Convert.ToInt32(Console.ReadLine());
+            Ges();
+        }
+    }
+}
+//consejo: no sirve para mas de 10 numeros
